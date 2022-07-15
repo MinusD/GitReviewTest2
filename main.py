0a2bcc2978da70dd9d1d7248005d5840896979e0
Eva <kapusteva@yandex.ru>
2022-07-15 18:18:01 +0300

    Вывод Ева

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def print_hi(name):
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('in main')
-
+def eva():
+    print ('eva')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
