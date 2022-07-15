ce383fd2a79fc842bd4848d82b8a4069410d8237
Eva <kapusteva@yandex.ru>
2022-07-15 18:21:56 +0300

    Исправлены отступы

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def print_hi(name):
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('in main')
+
+
 def eva():
-    print ('eva')
+    print('eva')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('in test')
     print_hi('PyCharm')
-
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
