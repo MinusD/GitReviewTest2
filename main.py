c384748fda739c0ce458d9c0de0c56f6256ed9d3
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-07-15 18:06:44 +0300

    Replace 321 to 123

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(321)
+    print(123)
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
